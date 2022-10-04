a841b0bac50d6fbe3df0f867a35ea72f9e7a69df
GuiSantosH <guiholm72@gmail.com>
2022-10-04 06:22:22 -0300

    Segundo exercicio

--- ExerciciosAula/Aula1/main.js
+++ ExerciciosAula/Aula1/main.js
@@ -1,1 +1,7 @@
 console.log("Olá mundo!"); //1º Exercicio
+
+function mostraNomeCompleto(nome, sobrenome) {
+    return nome + sobrenome;
+}
+
+console.log(mostraNomeCompleto('Guilherme ', 'Santos'));
