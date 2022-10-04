1e8d6abb24042ea01a1f1714b88045068905684f
GuiSantosH <guiholm72@gmail.com>
2022-10-04 06:36:07 -0300

    Quarto exercicio

--- ExerciciosAula/Aula1/main.js
+++ ExerciciosAula/Aula1/main.js
@@ -11,3 +11,15 @@ function calculaIMC(peso, altura) {
 }
 
 console.log(calculaIMC(80, 1.60))
+
+
+function calcNotas(nota1, nota2, nota3) {
+    const resultado = (nota1 + nota2 + nota3) / 3;
+    if(resultado >= 7){
+        console.log('Aprovado', resultado);
+    } else {
+        console.log('Reprovado', resultado);
+    }
+}
+
+calcNotas(50,80,40);
